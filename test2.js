7ff5ac7877635851c249a9a69810dc68ab7bfee9
Mohamedwalid89 <mohamed.walid.4836@gmail.com>
2025-09-05 22:58:48 +0300

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,9 @@
+// Create a student object
+let student = {
+    name: "Mohamed",
+    age: 19,
+    grade: 12
+};
+
+// Print the information
+console.log(`The student Mohamed is 19 years old and in grade 12.`);
